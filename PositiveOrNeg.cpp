c2471483980f40bb6582bc2f9f5dc3382c352575
Pulkit Bajpai <56193449+pulkitbajpaii@users.noreply.github.com>
2022-06-16 23:15:36 +0530

    Add files via upload

---
+++ PositiveOrNeg.cpp
@@ -0,0 +1,36 @@
+//1st method ;
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+
+if (n>=0)
+{
+    cout<<"+ve no.";
+}
+else
+{
+    cout<<"-ve";
+}
+
+return 0;
+
+}
+// 2nd method 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int num = -15;
+    
+//     //Condition to check if the 0, positive or negative
+    
+//     if(num == 0)
+//             cout << "Zero";
+//             else (num > 0) ? cout << "Positive": cout << "Negative";
+    
+//     return 0;
+// }
---
+++ evnOrodd.cpp
@@ -0,0 +1,21 @@
+// //1st method
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+// int n;
+// cin>>n;
+// if(n%2==0)
+// cout<<"even";
+// else
+// cout<<"odd";
+// }
+//2nd method
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+(n%2==0)? cout<<"even" : cout<<"odd";
+
+}
